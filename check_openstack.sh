055941da4ed5b5344ddaf81c0959192a0fe56e32
Vukasin <vukasin@atomia.com>
2012-01-12 14:14:13 +0100

    openstack script udpated.

--- check_openstack.sh
+++ check_openstack.sh
@@ -33,7 +33,7 @@ fi
 start_time=$(date +%s)
 
 # now we check status of machine
-machine_status=`euca-describe-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $5 }'`
+machine_status=`euca-describe-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $6 }'`
 machine_was_running="false"
 
 if [ -z "$machine_status" -o "$machine_status" != "running" ]
@@ -47,7 +47,7 @@ then
 		# give openstack some time to create machine
 		sleep 5
 		
-		machine_status=`euca-describe-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $5 }'`
+		machine_status=`euca-describe-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $6 }'`
 		if [ "$machine_status" = "running" ]
 		then
 			machine_was_running="true"
@@ -59,17 +59,17 @@ else
 fi
 
 # delete machine, no metter what is the status
-`euca-terminate-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $5 }'`
+`euca-terminate-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $6 }'`
 
 # sleep while machine is being terminated
-sleep 3
+sleep 20
 
 # make sure that machine is deleted
-machine_status=`euca-describe-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $5 }'`
+machine_status=`euca-describe-instances "$new_machine_id" -a "$ec2_access_key" -s "$ec2_secret_key" -U "$ec2_url" | grep INSTANCE | awk -F ' ' '{ print $6 }'`
 if [ -z "$machine_status" ]
 then
 	# machine deleted
-	if [ "$machine_was_running" = "true"]
+	if [ "$machine_was_running" = "true" ]
 	then
 		echo "OK: Openstack working."
 		exit 0
