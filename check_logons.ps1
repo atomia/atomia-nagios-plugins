8dc017525b19ba5f78ccb251662ea5390b5f34e6
Nikola Vitanovic <nikola.vitanovic@atomia.com>
2020-02-12 15:59:33 +0100

    Improved script to work with whitelist parameters

--- check_logons.ps1
+++ check_logons.ps1
@@ -1,42 +1,121 @@
+##########################################################################################
+#
+# Name:        check_logons.ps1
+# Date:        2020-02-11
+# Author:      nikola.vitanovic@atomia.com
+# Version:     1.0.0
+# Parameters:
+#              -logonTypes '10','3'
+#              -ignoreUsers 'VAGRANT\WINMASTER$','VAGRANT\Administrator'
+#              -ignoreIPs '127.0.0.1','192.168.33.10','192.168.33.22'
+#              -debug
+#
+# Returns:
+#              0 - OK
+#              2 - CRITICAL
+#              3 - UNKNOWN
+#
+# Description: Nagios plugin that alerts if there are 4624 EventIDs aka logins in the
+#              Security event log of the system.
+#
+#              In case there are unknown users CRITICAL message will be shown and exit
+#              code will be 2. Additional users with computer name or NetBIOS domain will
+#              be shown in the new lines after the CRITICAL message.
+#              In case there are unknown source IPs they will also be logged as CRITICAL.
+#
+#              The plugin logs a timestamp of last processed log in the TEMP folder. This
+#              timestamp is used to process only logs after the timestamp. Last processed
+#              log timestamp will be stored and all logs from that time forward will be
+#              processed. 
+#
+#              ATTENTION: You will get CRITICAL every next time the check occurs, after
+#              the first CRITICAL was encountered. Path to the file that should be deleted
+#              is shown in the CRITICAL Nagios message.
+#              
+#              All parameters are optional. Ignore parameters are essentially whitelist.
+#
+#              If you don't specify -logonTypes parameter only RDP logins will be checked.
+#              You can specify any LogonType. Valid types: https://bit.ly/2ULjexx
+#              
+#              If you don't specify -ignoreUsers any username will be treated as unknown.
+#              Usernames need to be specified in format "DOMAIN\Username" or
+#              "COMPUTER\Username". NetBIOS short name should be used.
+#
+#              If you don't specify -ignoreIPs any IP will be treated as suspcious.
+#              You can specify multiple IPs or only one. Any event that has different IP
+#              than the one available in the list will be reported as suspicious.
+#              Make sure that all IPs are specified.
+#
+#              If you specify -disableIPCheck IPs from the login events won't be checked,
+#              the parameter -ignoreIPs won't have any effect.
+#
+#              There is a -debug parameter that can be used to show more info about what
+#              events the script is currently processing and other details of the run.
+#
+##########################################################################################
+
 param (
-    [string[]]$logonTypes = @('10','11'),
-    [string[]]$ignore
+    [string[]]$logonTypes = @('10'),
+    [string[]]$ignoreUsers,
+    [string[]]$ignoreIPs,
+    [switch]$disableIPCheck,
+    [switch]$debug
 )
 
-#Get-EventLog -LogName Security -InstanceId 4624 -Newest 10 | Select-Object -Property *
-#exit 0
+$log = @()
+$lastLogLocation = "$env:TEMP\check_logons.LOG.lock"
+$lastCheckLocation = "$env:TEMP\check_logons.DONT-REMOVE.last"
 
-$listOfAdditionalIPs = @()
-$listOfAdditionalIndexes = @()
-$listOfAdditionalUsernames = @()
-$logLocation = "$env:TEMP\check_logons.last"
+# Show temp location in case DEBUG mode is on
+if($debug)
+{
+    Write-Host "DEBUG: Temp location $env:TEMP"
+}
+
+# Check if the lock file exists with the log.
+# Show that log and exit as CRITICAL.
+if(Test-Path $lastLogLocation)
+{
+    $output = Get-Content -Path $lastLogLocation -Encoding UTF8 -Raw
+    Write-Host $output
+    Write-Host "Check out the activity and remove $lastLogLocation to recover"
+    exit 2
+}
 
 # Load the file that contains the last timestamp.
 # If there is an issue assume that current time
 # is the last time checked.
 $lastCheck = Get-Date -Format "yyyy-MM-dd HH:mmm:ssK"
-echo $lastCheck
 try
 {
-    $lastCheck = Get-Content -Path $logLocation
+    if(!(Test-Path $lastCheckLocation))
+    {
+        throw "Does not exist"
+    }
+    
+    $lastCheck = Get-Content -Path $lastCheckLocation
     if([string]::IsNullOrEmpty($lastCheck))
     {
-        throw
+        throw "Empty lastlog"
     }
 }
 catch
 {
+    # Write current time as last event time, new events will be
+    # parsed from this current time. This happens because the
+    # needed file does not exist or is empty and needs to be
+    # initialized in order for the app to work.
     $lastCheck = Get-Date -Format "yyyy-MM-dd HH:mmm:ssK"
-    Out-File -FilePath $logLocation -InputObject $lastCheck
+    Out-File -FilePath $lastCheckLocation -InputObject $lastCheck
 }
 
-Write-Host $logLocation
-$var = Get-EventLog -LogName Security -InstanceId 4624 -After $lastCheck | Select Message,ReplacementStrings,TimeGenerated,Index
+# Gather the Security event logs from the DB and process them.
+$eventLogs = Get-EventLog -LogName Security -InstanceId 4624 -After $lastCheck | Select Message,ReplacementStrings,TimeGenerated,Index
 $numberOfProcessedLogs = 0
 
-foreach($item in $var)
+foreach($item in $eventLogs)
 {
-    # Gather the data from the log entry that is needed
+    # Gather the data from the log entry that is needed.
     $currentTime = $item.TimeGenerated.ToString("yyyy-MM-dd HH:mmm:ssK")
     $currentIp = $item.ReplacementStrings[18]
     $currentUser = $item.ReplacementStrings[5]
@@ -44,11 +123,42 @@ foreach($item in $var)
     $currentComputerNameDomain = $item.ReplacementStrings[6]
     $currentIndex = $item.Index
 
-    Write-Host $currentIndex $currentTime $currentIp $currentUser $currentLogonType
+    if($debug)
+    {
+        Write-Host "DEBUG: EventIndex: $currentIndex Time: $currentTime IP: $currentIp User: $currentUser LogonType: $currentLogonType"
+    }
 
+    # Check if the logged in account is specific type.
     if($logonTypes -contains $currentLogonType)
     {
-        Write-Host "Login detected - User: $currentComputerNameDomain\$currentUser IP: $currentIp Index: $currentIndex"
+        # Logic to check if the users is whitelisted or not.
+        if($ignoreUsers -contains "$currentComputerNameDomain\$currentUser")
+        {
+            if($debug)
+            {
+                Write-Host "DEBUG: User $currentComputerNameDomain\$currentUser is in the list"
+            }
+        }
+        else
+        {
+            $log += "Suspicious user - User: $currentComputerNameDomain\$currentUser IP: $currentIp EventIndex: $currentIndex LogonType: $currentLogonType"
+        }
+        
+        # Logic to check if the IP is whitelisted or not.
+        if($disableIPCheck)
+        {
+            if($ignoreIPs -contains $currentIp)
+            {
+                if($debug)
+                {
+                    Write-Host "DEBUG: IP $currentIp is in the list"
+                }
+            }
+            else
+            {
+                $log += "Suspicious IP - User: $currentComputerNameDomain\$currentUser IP: $currentIp EventIndex: $currentIndex LogonType: $currentLogonType"
+            }
+        }
     }
 
     # Since the logs are sorted from newest to the oldest we need to
@@ -56,9 +166,25 @@ foreach($item in $var)
     # This helps us to parse only unparsed logs.
     if($numberOfProcessedLogs -eq 0)
     {
-        Out-File -FilePath $logLocation -InputObject $currentTime
+        Out-File -FilePath $lastCheckLocation -InputObject $currentTime
     }
     $numberOfProcessedLogs++
 }
 
+# Display CRITICAL if there are unauthorised events and log it into a file.
+if($log.Count -gt 0)
+{
+    $unauthorisedLogs = $log.Count
+    $criticalMessage = "CRITICAL - There are $unauthorisedLogs unauthorised logins"
+    foreach($item in $log)
+    {
+        $criticalMessage += "`r`n$item"
+    }
+    Write-Host $criticalMessage
+    Out-File -FilePath $lastLogLocation -InputObject $criticalMessage
+    exit 2
+}
+
+# Display OK if no suspicious activity was detected.
 Write-Host "OK - Processed $numberOfProcessedLogs logs"
+exit 0
